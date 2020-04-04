6dc5a3c0423ad2ebd271f68a21bfebe0d2f1b037
Thyago Volpatto <tvolpatto@gmail.com>
2020-04-04 13:08:52 -0600

    created service-work

---
+++ public/service-worker.js
@@ -0,0 +1,19 @@
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/index.js',
+    '/styles.css'
+];
+
+const CACHE_NAME = "static-cache-v2";
+
+self.addEventListener("install", function (evt) {
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+
+    self.skipWaiting();
+});
+
